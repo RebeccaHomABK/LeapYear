9dd983e78dc8b52080f859af76b06df95fb3f44c
RebeccaHomABK <109548240+RebeccaHomABK@users.noreply.github.com>
2022-07-19 16:38:40 -0700

    Add project files.

---
+++ LeapYear/LeapYear.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main()
+{
+    //  Gets the year.
+    std::cout << "Enter a year: ";
+    int year;
+    std::cin >> year;
+
+    //  Checks if the year is a leap year.
+    bool isLeapYear = (year % 4) == 0;
+    
+    //  Checks if the year is a multiple of 100.
+    if (isLeapYear && ((year % 100) == 0))
+    {
+        isLeapYear = false;
+    }
+    if (isLeapYear || (year % 400) == 0)
+    {
+        std::cout << year << " is a leap year." << std::endl;
+    }
+    else
+    {
+        std::cout << year << " is not a leap year." << std::endl;
+    }
+}
