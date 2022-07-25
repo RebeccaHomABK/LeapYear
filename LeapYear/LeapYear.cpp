2ff7b911d7e0160eb06cbc309386ad8c2bab23ba
RebeccaHomABK <109548240+RebeccaHomABK@users.noreply.github.com>
2022-07-25 10:19:25 -0700

    updated leap year if condition

--- LeapYear/LeapYear.cpp
+++ LeapYear/LeapYear.cpp
@@ -8,15 +8,8 @@ int main()
     std::cin >> year;
 
     //  Checks if the year is a leap year.
-    if ((year % 400) == 0)
-    {
-        std::cout << year << " is a leap year." << std::endl;
-    }
-    else if ((year % 100) == 0)
-    {
-        std::cout << year << " is not a leap year." << std::endl;
-    }
-    else if ((year % 4) == 0)
+    if (((year % 400) == 0) 
+        || (((year % 4) == 0) && !((year % 100) == 0)))
     {
         std::cout << year << " is a leap year." << std::endl;
     }
